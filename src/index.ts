00ed3d635dad887601827765002b6f7741d612d8
Dmitry Sadovsky <dimailn9@gmail.com>
2021-12-03 17:40:37 +0300

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import {Effect} from 'vrf'
+import urljoin from 'url-join'
+
+
+export default (
+  {
+    redirectTo = (path, form) => form.$router.replace(path),
+    prefix = (form) => ""
+  } :  {
+    redirectTo: (path: string, form: any) => void,
+    prefix: (form: any) => string
+  }
+) : Effect => {
+  return {
+    name: 'redirect',
+    effect({onCreated, form, strings: { urlResourceCollectionName}}){
+      onCreated((event) => {
+        event.stopPropagation()
+
+        const {payload: {id}} = event
+
+        redirectTo(
+          urljoin(
+            prefix(form),
+            urlResourceCollectionName(),
+            String(id)
+          ),
+          form
+        )
+      })
+    }
+  }
+}
